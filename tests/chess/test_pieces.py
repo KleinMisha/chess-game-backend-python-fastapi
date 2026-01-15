7c6e5f683296e57787a5feb5be8de3dc17c6fbb2
KleinMisha <klein.misha@gmail.com>
2026-01-15 17:15:11 +0100

    Test promotion logic.

--- tests/chess/test_pieces.py
+++ tests/chess/test_pieces.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from src.chess.pieces import PIECE_FEN, Color, Piece
+from src.chess.pieces import PIECE_FEN, Color, Piece, PieceType
 
 
 @pytest.mark.parametrize("char", [char.upper() for char in PIECE_FEN.keys()])
@@ -19,3 +19,12 @@ def test_creating_black_piece_from_fen(char: str) -> None:
     piece = Piece.from_fen(char)
     assert piece.type == PIECE_FEN[char.lower()]
     assert piece.color == Color.BLACK
+
+
+@pytest.mark.parametrize("color", [c for c in Color])
+def test_promotion_to_queen(color: Color) -> None:
+    """Promote a piece to Queen. Make sure that the class is mutable (and not changed to frozen or something), and does not by accident change the color"""
+    piece = Piece(PieceType.PAWN, color)
+    piece.promote_to(PieceType.QUEEN)
+    assert piece.type == PieceType.QUEEN
+    assert piece.color == color
