5a3201c51e2d0da75cdbfd1c493b69a35ed54870
KleinMisha <klein.misha@gmail.com>
2026-01-19 01:15:58 +0100

    Created inverse dictionary for convenience. Can now lookup FEN character for a given piece type.

--- tests/chess/test_pieces.py
+++ tests/chess/test_pieces.py
@@ -2,22 +2,22 @@
 
 import pytest
 
-from src.chess.pieces import PIECE_FEN, Color, Piece, PieceType
+from src.chess.pieces import FEN_TO_PIECE, Color, Piece, PieceType
 
 
-@pytest.mark.parametrize("char", [char.upper() for char in PIECE_FEN.keys()])
+@pytest.mark.parametrize("char", [char.upper() for char in FEN_TO_PIECE.keys()])
 def test_creating_white_piece_from_fen(char: str) -> None:
     """Capital letters are used for white pieces"""
     piece = Piece.from_fen(char)
-    assert piece.type == PIECE_FEN[char.lower()]
+    assert piece.type == FEN_TO_PIECE[char.lower()]
     assert piece.color == Color.WHITE
 
 
-@pytest.mark.parametrize("char", [char.lower() for char in PIECE_FEN.keys()])
+@pytest.mark.parametrize("char", [char.lower() for char in FEN_TO_PIECE.keys()])
 def test_creating_black_piece_from_fen(char: str) -> None:
     """Lower case letters are used for white pieces"""
     piece = Piece.from_fen(char)
-    assert piece.type == PIECE_FEN[char.lower()]
+    assert piece.type == FEN_TO_PIECE[char.lower()]
     assert piece.color == Color.BLACK
 
 
