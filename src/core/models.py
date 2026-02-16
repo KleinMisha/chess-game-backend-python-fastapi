5039285fbcfc359fea1a1d8a09c08f4f03181edc
KleinMisha <klein.misha@gmail.com>
2026-02-17 00:16:52 +0100

    updated description.

--- src/core/models.py
+++ src/core/models.py
@@ -1,19 +1,24 @@
 """
-Contract for the Service layer.
+Boundary layer data model(s).
 
-Domain level data model of information representing a Game.
-
+These objects can be used to communicate with the Service.
+Hence, both the API layer (higher) and domain/db layers (lower) will use model(s) defined here to send to/receive from the Service
+(Decouples the data model specific to the DB layer, API layer, or domain layer from the information needed to send across boundaries)
 """
 
 from dataclasses import dataclass
 
+# Type aliases to make GameModel easier to read
+PieceColor = str
+PlayerName = str
+
 
 @dataclass
 class GameModel:
-    """Chess specific data + game handling info (players, etc.)"""
+    """Transport-safe representation of a chess game used between API, Service, DB, and Game layers."""
 
     current_fen: str
     history_fen: list[str]
     moves_uci: list[str]
-    registered_players: dict[str, str]
+    registered_players: dict[PieceColor, PlayerName]
     status: str
