03eee05589124df69c43e5440dad97079585972a
KleinMisha <klein.misha@gmail.com>
2026-02-24 15:43:52 +0100

    moved exception handling (FastAPI specific stuff) into main.py

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -1,15 +1,13 @@
 """Custom exception hierarchy"""
 
-from fastapi import Request
-from fastapi.responses import JSONResponse
-
 
 class BaseError(Exception):
     """Custom root exception."""
 
 
 # --- Domain (Game logic) ---
-class GameError(BaseError): ...
+class GameError(BaseError):
+    """Invalid game logic."""
 
 
 class InvalidFENError(GameError): ...
@@ -28,38 +26,11 @@ class GameCreationError(GameError): ...
 
 
 # -- Application (app orchestration, repository, request validation, ...)
-class AppError(BaseError): ...
+class AppError(BaseError):
+    """Application failed to orchestrate communication between Game, repository and API."""
 
 
 class InvalidRequestError(AppError): ...
 
 
 class GameNotFoundError(AppError): ...
-
-
-# --- EXCEPTION HANDLER (FastAPI) ---
-HTTP_STATUS_CODES: dict[type[BaseError], int] = {
-    InvalidFENError: 400,
-    IllegalMoveError: 400,
-    GameStateError: 409,
-    NotYourTurnError: 409,
-    GameCreationError: 400,
-    InvalidRequestError: 400,
-    GameNotFoundError: 404,
-}
-
-
-def exception_handler(_: Request, exception: BaseError) -> JSONResponse:
-    """FastAPI's mechanism for handling exceptions. Will attach to FastAPI instance in main.py entry point."""
-    status_code = next(
-        (
-            code
-            for exc_type, code in HTTP_STATUS_CODES.items()
-            if isinstance(exception, exc_type)
-        ),
-        400,  # default value
-    )
-    return JSONResponse(
-        status_code=status_code,
-        content={"error": type(exception).__name__, "message": str(exception)},
-    )
